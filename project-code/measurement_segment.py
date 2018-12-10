843c1352882f9fdfb2eba5f9833a4ee570408b80
Gregor von Laszewski <laszewski@gmail.com>
2018-12-10 13:45:26 -0500

    update measurement

--- project-code/measurement_segment.py
+++ project-code/measurement_segment.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec  8 14:00:34 2018
@@ -6,12 +7,13 @@ Created on Sat Dec  8 14:00:34 2018
 """
 
 import cv2
-import matplotlib.pyplot as plt
+import glob
 
 x0,x1,y0,y1 = 1280, 640, 180, 400
 delta_x = -1
-def image_segments(frame):
-    img = cv2.imread(frame)
+
+def image_segments(image):
+    img = cv2.imread(image)
     flag = False
     for x in range(x0,x1, delta_x):
         mini_img = img[y0:y1,x:(x+160),:]
@@ -28,3 +30,15 @@ def image_segments(frame):
             axis = x
             break
     mini_secchi = img[:,axis-400:axis-90,:]
+
+
+
+# main
+if __name__== "__main__":
+    files = sorted(glob.glob("images/*.jpg"))
+    print (files)
+    for file in files:
+        print (file)
+        data = image_segments(file)
+        print (file, data)
+    
